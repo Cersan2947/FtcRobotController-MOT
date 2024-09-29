e907580c75a7c569f4a43ab447f8c33a7dbd5598
IKKNIGHT <76535471+IKKNIGHT@users.noreply.github.com>
2024-09-29 13:52:30 -0500

    Made Field Centric code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fieldcentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Fieldcentric.java
@@ -67,6 +67,7 @@ public class Fieldcentric extends LinearOpMode {
         double frPower = (power * sin / maxSincos - turn);
         double blPower = (power * sin / maxSincos + turn);
         double brPower = (power * cos / maxSincos - turn);
+        double brPowertest = (power * cos / maxSincos - turn);
 
 
 
